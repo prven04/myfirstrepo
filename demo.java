e97d3a6f8bd3ed707f8fa5f67e342464ac377a60
prven04 <prven04@gmail.com>
2018-02-17 06:33:44 +0000

    java within stash

--- demo.java
+++ demo.java
@@ -1,8 +1,15 @@
+fundtransfer()
+{
+}
+deposit()
+{
+}
 class newfile
 {
 }
 psvm(string[]  args)
 {
 sop("already demo.java is deleted now i create again as a new file");
+sop( in stash 2nd time i open this)
 }
 
